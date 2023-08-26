bd613999fb6158c9a861b0ebb2075c4cc8daa9f0
ymtech <younes.manjal@gmail.com>
2023-08-26 15:36:08 +0200

    docs(types): add jsdoc

--- src/types/QuadrilateralTypes.ts
+++ src/types/QuadrilateralTypes.ts
@@ -1,6 +1,35 @@
+/**
+ * Interface representing a Quad object.
+ *
+ * @interface IQuad
+ */
 export interface IQuad {
+    /**
+     * The starting x-coordinate for the Quad.
+     *
+     * @type {number}
+     */
     startX: number;
+
+    /**
+     * The starting y-coordinate for the Quad.
+     *
+     * @type {number}
+     */
     startY: number;
+
+    /**
+     * The current HTMLDivElement being used for the Quad.
+     * Set to `null` if no div is currently associated.
+     *
+     * @type {HTMLDivElement | null}
+     */
     currentQuad: HTMLDivElement | null;
+
+    /**
+     * Flag indicating whether the Quad is currently being drawn.
+     *
+     * @type {boolean}
+     */
     drawing: boolean;
 }
