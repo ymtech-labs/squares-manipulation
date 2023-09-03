d75fd122d14dbd20bc43c0cffacc4d4dbdd4b96e
ymtech <younes.manjal@gmail.com>
2023-09-03 21:06:50 +0200

    refactor(path-aliases ): change config path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { configDefaults } from "vitest/dist/config";
 
 export default defineConfig({
+    plugins: [tsconfigPaths()],
     test: {
         globals: true,
         environmentMatchGlobs: [["**/*.dom.*", "happy-dom"]],
